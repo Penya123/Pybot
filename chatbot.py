6bf7808a8fa8b1f58f3083bd0264461179865e8b
Jorge Sandoval <jorgesagt5@gmail.com>
2026-04-26 17:02:25 -0700

    The connection and implementation with the Gemini AI model was successful

--- chatbot.py
+++ chatbot.py
@@ -1,13 +1,35 @@
-# import os
-# from dotenv import load_dotenv
-# from google import genai
+import os
+from dotenv import load_dotenv
+from google import genai
+from google.genai import types
 
-# load_dotenv()
+load_dotenv()
 
-# client = genai.Client(api_key=os.environ.get("GEMINI_API_KEY"))
+client = genai.Client(api_key=os.environ.get("GEMINI_API_KEY"))
 
-# def sayHello():
-#     response = client.models.generate_content(
-#         model="gemini-3-flash-preview", contents="Saludo corto"
-#     )
-#     print(response.text)
+
+def sayHello():
+    response = client.models.generate_content(
+        model = "gemini-3-flash-preview", 
+        contents = "Short greeting",
+        config = types.GenerateContentConfig(
+            system_instruction = "You´re a friendly assistant, who might be asked about proggramming questions.",
+            temperature = 1,
+            max_output_tokens = 100,
+            top_p = 0.8,
+        )
+    )
+    return response.text
+
+def conversation(prompt):
+    response = client.models.generate_content(
+        model = "gemini-3-flash-preview", 
+        contents = "Make sure to give a short and direct answer(less than 750 tokens). This is the prompt: " + prompt,
+        config = types.GenerateContentConfig(
+            system_instruction = "You´re a friendly assistant, who might be asked about proggramming questions.",
+            temperature = 1,
+            max_output_tokens = 750,
+            top_p = 0.8,
+        )
+    )
+    return response.text
--- main.py
+++ main.py
@@ -2,13 +2,20 @@ import chatbot as Pybot
 import database 
 from datetime import date, time, datetime
 
+def chat():
+    response = input("Our little ai says\n#: " + Pybot.sayHello() + "\n$:")
+    while True:
+        response = input("Our little ai says\n#: " + Pybot.conversation(response) + "\n$:")
+
+
+
 print("██████╗░██╗░░░██╗██████╗░░█████╗░████████╗")
 print("██╔══██╗╚██╗░██╔╝██╔══██╗██╔══██╗╚══██╔══╝")
 print("██████╔╝░╚████╔╝░██████╦╝██║░░██║░░░██║░░░")
 print("██╔═══╝░░░╚██╔╝░░██╔══██╗██║░░██║░░░██║░░░")
 print("██║░░░░░░░░██║░░░██████╦╝╚█████╔╝░░░██║░░░")
 print("╚═╝░░░░░░░░╚═╝░░░╚═════╝░░╚════╝░░░░╚═╝░░░")
-print("A little dummy chatbot made in python,\ngo ahead and ask him something about programming :3")
+print("A little dummy chatbot made in python!\ngo ahead and ask him something about programming :3\n")
 
 opc = input("What you wanna do? \n" \
             "[1]Chat with him!\n" \
@@ -17,9 +24,13 @@ opc = input("What you wanna do? \n" \
 match opc:
     case "1":
         print("Go on and talk to him")
+        chat()
     case "2":
         print("This dump app was made by Jorge Luis.\n" \
         "I built this using the gemini 3 flash preview model\n" \
         "connecting it with its API given free by Google AI Studio.")
     case _:
         print("I guess you want to talk to him anyways")
+        chat()
+
+
