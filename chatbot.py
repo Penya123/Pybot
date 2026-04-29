7ef03a158a656b507922122acad6f354eae62788
Jorge Sandoval <jorgesagt5@gmail.com>
2026-04-28 22:50:21 -0700

    The charbot script was updated implementing the new database fuctions to store and give to Gemini the history

--- chatbot.py
+++ chatbot.py
@@ -3,33 +3,58 @@ from dotenv import load_dotenv
 from google import genai
 from google.genai import types
 
+import database
+
 load_dotenv()
 
 client = genai.Client(api_key=os.environ.get("GEMINI_API_KEY"))
 
+CONFIG = types.GenerateContentConfig(
+    system_instruction="You're a friendly assistant focused on programming questions.",
+    temperature=1,
+    max_output_tokens=750,
+    top_p=0.8,
+)
 
 def sayHello():
     response = client.models.generate_content(
         model = "gemini-3-flash-preview", 
         contents = "Short greeting",
-        config = types.GenerateContentConfig(
-            system_instruction = "You´re a friendly assistant, who might be asked about proggramming questions.",
-            temperature = 1,
-            max_output_tokens = 100,
-            top_p = 0.8,
-        )
+        config = CONFIG
     )
     return response.text
 
 def conversation(prompt):
+    database.save_message(
+        "usuario",  #Sender
+        prompt,     #Prompt
+        database.get_next_number() #Number of the message
+    )
+
+    #Brings up the last 3 messages, including the last one
+    history = database.get_last_messages(limit=6)
+
+    #Convert the history into the format required by Gemini
+    contents = []
+    for msg in history:
+        role = "user" if msg["remitente"] == "usuario" else "model"
+        contents.append(
+            types.Content(
+                role=role,
+                parts=[types.Part(text=msg["mensaje"])]
+            )
+        )
+
+
+    #Call the model 
     response = client.models.generate_content(
         model = "gemini-3-flash-preview", 
-        contents = "Make sure to give a short and direct answer(less than 750 tokens). This is the prompt: " + prompt,
-        config = types.GenerateContentConfig(
-            system_instruction = "You´re a friendly assistant, who might be asked about proggramming questions.",
-            temperature = 1,
-            max_output_tokens = 750,
-            top_p = 0.8,
-        )
+        contents = contents,
+        config = CONFIG
     )
-    return response.text
+    answer = response.text
+
+    #Save response in the database
+    database.save_message("modelo", answer, database.get_next_number() + 1)
+
+    return answer
