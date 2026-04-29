a154fc868d150714c7bf9794b28f5bc6027936d0
Jorge Sandoval <jorgesagt5@gmail.com>
2026-04-28 22:32:05 -0700

    The get_next_number fuction was made

--- database.py
+++ database.py
@@ -23,4 +23,6 @@ def save_message(sender, message, number):
 def get_last_messages(limit = 3):
     return list(reversed(list(chats.find().sort("numero", -1).limit(limit)))) 
 
-
+def get_next_number():
+    last = chats.find_one(sort=[("numero", - 1)])
+    return (last["numero"] + 1) if last else 0
